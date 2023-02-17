7deb09001cebfe67c03fb2ada1822ab5bde38ce0
Christopher Kennedy <kennednu@bc.edu>
2023-02-16 21:54:25 -0500

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by Christopher Kennedy on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
